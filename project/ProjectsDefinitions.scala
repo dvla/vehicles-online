205c66b8879923965f6205f471fb861c4139ec8d
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-10-16 16:11:51 +0100

    Update the sandbox to the latest microservices versions

--- project/ProjectsDefinitions.scala
+++ project/ProjectsDefinitions.scala
@@ -2,9 +2,9 @@ import sbt.Keys.{libraryDependencies, resolvers}
 import sbt._
 
 object ProjectsDefinitions {
-  final val VersionOsAddressLookup = "0.1-SNAPSHOT"
-  final val VersionVehiclesLookup = "0.1-SNAPSHOT"
-  final val VersionVehiclesDisposeFulfil = "0.1-SNAPSHOT"
+  final val VersionOsAddressLookup = "0.3-SNAPSHOT"
+  final val VersionVehiclesLookup = "0.3-SNAPSHOT"
+  final val VersionVehiclesDisposeFulfil = "0.3-SNAPSHOT"
   final val VersionLegacyStubs = "1.0-SNAPSHOT"
   final val VersionJetty = "9.2.1.v20140609"
   final val VersionSpringWeb = "3.0.7.RELEASE"
