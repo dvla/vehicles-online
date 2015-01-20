e3765facff1af2f55b1748a3f3c3671a10290597
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-01-20 12:48:23 +0000

    DE289: Remove cookies when in duplicate error page.

--- app/controllers/DuplicateDisposalError.scala
+++ app/controllers/DuplicateDisposalError.scala
@@ -3,13 +3,15 @@ package controllers
 import javax.inject.Inject
 import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichResult
 import utils.helpers.Config
+import models.DisposeCacheKeys
 
 final class DuplicateDisposalError @Inject()()
                                    (implicit clientSideSessionFactory: ClientSideSessionFactory, config: Config)
   extends Controller {
 
   def present = Action { implicit request =>
-    Ok(views.html.disposal_of_vehicle.duplicate_disposal_error())
+    Ok(views.html.disposal_of_vehicle.duplicate_disposal_error()).discardingCookies(DisposeCacheKeys)
   }
 }
