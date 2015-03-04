e0fd4688022319055de58cc57420d264cf309067
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-03-04 14:53:18 +0000

    New development version 1.4.6-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "1.4.5"
+  val versionString = "1.4.6-SNAPSHOT"
   val scalaVersionString = "2.10.3"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
