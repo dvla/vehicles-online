20a22bac0f1c71a250553a2c1933cd5b63e6e0ee
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-03-27 14:10:25 +0000

    US2666 adding a private protected val sellNewVehicleCall = controllers.routes.VehicleLookup.present()

---
+++ app/controllers/priv/DisposeFailure.scala
@@ -0,0 +1,13 @@
+package controllers.priv
+
+import com.google.inject.Inject
+import controllers.routes
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class DisposeFailure @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                                 config: Config) extends controllers.DisposeFailure {
+  protected override val sellNewVehicleCall = controllers.routes.VehicleLookup.present()
+  protected override val exitCall = controllers.routes.SetUpTradeDetails.present()
+  protected override val onMissingCookies = Redirect(routes.SetUpTradeDetails.present())
+}
