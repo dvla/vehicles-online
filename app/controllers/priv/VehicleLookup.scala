fa396f993d90ea47fdc48103247966022d7b1c3d
Ian Stainer <ian.stainer@valtech.co.uk>
2015-03-27 17:02:38 +0000

    US2666 Abstracted out the calls for vehicle lookup

--- app/controllers/priv/VehicleLookup.scala
+++ app/controllers/priv/VehicleLookup.scala
@@ -1,7 +1,7 @@
 package controllers.priv
 
 import com.google.inject.Inject
-import controllers.{routes, SurveyUrl}
+import controllers.SurveyUrl
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.services.DateService
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.bruteforceprevention.BruteForcePreventionService
@@ -17,14 +17,14 @@ class VehicleLookup @Inject()(implicit bruteForceService: BruteForcePreventionSe
 
   protected override val submitTarget = controllers.routes.VehicleLookup.submit()
   protected override val exitTarget = controllers.routes.VehicleLookup.exit()
-  protected override val onVrmLocked = Redirect(routes.VrmLocked.present())
-  protected override val onMicroServiceError = Redirect(routes.MicroServiceError.present())
-  protected override val onVehicleLookupFailure = Redirect(routes.VehicleLookupFailure.present())
-  protected override val missingTradeDetails = Redirect(routes.SetUpTradeDetails.present())
-  protected override val enterAddressManually = Redirect(routes.EnterAddressManually.present())
-  protected override val businessChooseYourAddress = Redirect(routes.BusinessChooseYourAddress.present())
-  protected override val suppressedV5C = Redirect(routes.SuppressedV5C.present())
-  protected override val duplicateDisposalError = Redirect(routes.DuplicateDisposalError.present())
-  protected override val dispose = Redirect(routes.Dispose.present())
+  protected override val onVrmLocked = Redirect(controllers.routes.VrmLocked.present())
+  protected override val onMicroServiceError = Redirect(controllers.routes.MicroServiceError.present())
+  protected override val onVehicleLookupFailure = Redirect(controllers.routes.VehicleLookupFailure.present())
+  protected override val missingTradeDetails = Redirect(controllers.routes.SetUpTradeDetails.present())
+  protected override val enterAddressManually = Redirect(controllers.routes.EnterAddressManually.present())
+  protected override val businessChooseYourAddress = Redirect(controllers.routes.BusinessChooseYourAddress.present())
+  protected override val suppressedV5C = Redirect(controllers.routes.SuppressedV5C.present())
+  protected override val duplicateDisposalError = Redirect(controllers.routes.DuplicateDisposalError.present())
+  protected override val dispose = Redirect(controllers.routes.Dispose.present())
   protected override val onExit = Redirect(config.endUrl)
 }
