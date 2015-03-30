bb3e6961597213a2fa2a0b346a281735c441b9a0
Ian Stainer <ian.stainer@valtech.co.uk>
2015-03-30 10:54:17 +0100

    US2666 abstracted out the routs for the MicroServiceError controller

--- app/controllers/MicroServiceError.scala
+++ app/controllers/MicroServiceError.scala
@@ -1,21 +1,23 @@
 package controllers
 
 import com.google.inject.Inject
-import controllers.routes.VehicleLookup
+import models.DisposeCacheKeyPrefix.CookiePrefix
 import models.DisposeFormModel.PreventGoingToDisposePageCacheKey
 import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.{RichCookies, RichResult}
 import utils.helpers.Config
-import models.DisposeCacheKeyPrefix.CookiePrefix
 
 class MicroServiceError @Inject()(implicit clientSideSessionFactory: ClientSideSessionFactory,
-                                        config: Config) extends Controller {
-  private final val DefaultRedirectUrl = VehicleLookup.present().url
+                                  config: Config) extends Controller {
+  
+  protected val defaultRedirectUrl = controllers.routes.VehicleLookup.present().url
+  protected val tryAgainTarget = controllers.routes.MicroServiceError.back()
+  protected val exitTarget = controllers.routes.BeforeYouStart.present()
 
   def present = Action { implicit request =>
-    val referer = request.headers.get(REFERER).getOrElse(DefaultRedirectUrl)
-    ServiceUnavailable(views.html.disposal_of_vehicle.micro_service_error()).
+    val referer = request.headers.get(REFERER).getOrElse(defaultRedirectUrl)
+    ServiceUnavailable(views.html.disposal_of_vehicle.micro_service_error(tryAgainTarget, exitTarget)).
       // Save the previous page URL (from the referer header) into a cookie.
       withCookie(MicroServiceError.MicroServiceErrorRefererCacheKey, referer).
       // Remove the interstitial cookie so we do not get bounced back to vehicle lookup unless we were on that page
@@ -23,7 +25,7 @@ class MicroServiceError @Inject()(implicit clientSideSessionFactory: ClientSideS
   }
 
   def back = Action { implicit request =>
-    val referer: String = request.cookies.getString(MicroServiceError.MicroServiceErrorRefererCacheKey).getOrElse(DefaultRedirectUrl)
+    val referer: String = request.cookies.getString(MicroServiceError.MicroServiceErrorRefererCacheKey).getOrElse(defaultRedirectUrl)
     Redirect(referer).discardingCookie(MicroServiceError.MicroServiceErrorRefererCacheKey)
   }
 }
---
+++ app/controllers/priv/MicroServiceError.scala
@@ -0,0 +1,13 @@
+package controllers.priv
+
+import com.google.inject.Inject
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class MicroServiceError @Inject()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                                  config: Config) extends controllers.MicroServiceError {
+
+  protected override val defaultRedirectUrl = controllers.routes.VehicleLookup.present().url
+  protected override val tryAgainTarget = controllers.routes.MicroServiceError.back()
+  protected override val exitTarget = controllers.routes.BeforeYouStart.present()
+}
