f21b774240074dc9c25939ecb5780c1a29862adc
Ian Stainer <ian.stainer@valtech.co.uk>
2015-03-30 11:19:45 +0100

    US2666 abstracted out the routs for the Help controller

--- app/controllers/Help.scala
+++ app/controllers/Help.scala
@@ -8,17 +8,19 @@ import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicit
 import utils.helpers.Config
 
 class Help @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
-                             config: Config) extends Controller {
+                       config: Config) extends Controller {
+
+  protected val backTarget = controllers.routes.Help.back()
+  protected val exitTarget = controllers.routes.BeforeYouStart.present()
 
   def present = Action { implicit request =>
     val origin = request.headers.get(REFERER).getOrElse("No Referer in header")
-    Ok(views.html.common.help()).
+    Ok(views.html.common.help(backTarget, exitTarget)).
       withCookie(HelpCacheKey, origin) // Save the previous page URL (from the referer header) into a cookie.
   }
 
   def back = Action { implicit request =>
     val origin: String = request.cookies.getString(HelpCacheKey).getOrElse(config.startUrl)
-    Redirect(origin).
-      discardingCookie(HelpCacheKey)
+    Redirect(origin).discardingCookie(HelpCacheKey)
   }
 }
---
+++ app/controllers/priv/Help.scala
@@ -0,0 +1,12 @@
+package controllers.priv
+
+import com.google.inject.Inject
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class Help @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                       config: Config) extends controllers.Help {
+
+  protected override val backTarget = controllers.routes.Help.back()
+  protected override val exitTarget = controllers.routes.BeforeYouStart.present()
+}
