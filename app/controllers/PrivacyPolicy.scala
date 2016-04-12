03218bbfdcafa963f3f072aaaa0b076c60a46a06
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-04-12 10:01:38 +0100

    VMPRCI-2861 Privacy Policy page

---
+++ app/controllers/PrivacyPolicy.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc.Action
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class PrivacyPolicy @Inject()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                             config: Config)
+                             extends BusinessController {
+
+  def present = Action { implicit request =>
+    Ok(views.html.disposal_of_vehicle.privacy_policy())
+  }
+}
