9275a53482a6b267efea2f9077ce005debd62485
Ian Stainer <ian.stainer@valtech.co.uk>
2016-08-12 10:22:03 +0100

    VMPRCI-4490 updated version information so version information is displayed alphabetically for all dependent micro services

--- app/controllers/Version.scala
+++ app/controllers/Version.scala
@@ -2,15 +2,19 @@ package controllers
 
 import com.google.inject.Inject
 import uk.gov.dvla.vehicles.presentation.common.controllers
+import uk.gov.dvla.vehicles.presentation.common.controllers.Version.Suffix
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.addresslookup.ordnanceservey.OrdnanceSurveyConfig
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.emailservice.EmailServiceConfig
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup.VehicleAndKeeperLookupConfig
 import webserviceclients.dispose.DisposeConfig
 
 class Version @Inject()(vehicleAndKeeperLookupConfig: VehicleAndKeeperLookupConfig,
                         osAddressLookupConfig: OrdnanceSurveyConfig,
-                        disposeConfig: DisposeConfig)
+                        vehiclesDisposeConfig: DisposeConfig,
+                        emailConfig: EmailServiceConfig)
   extends controllers.Version(
-    osAddressLookupConfig.baseUrl + "/version",
-    vehicleAndKeeperLookupConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + "/version",
-    disposeConfig.baseUrl + "/version"
+    emailConfig.emailServiceMicroServiceBaseUrl + Suffix,
+    osAddressLookupConfig.baseUrl + Suffix,
+    vehicleAndKeeperLookupConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
+    vehiclesDisposeConfig.baseUrl + Suffix
   )
