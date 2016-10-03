b45aaa268c744698b3ec153033d5173a996ee1ce
Ian Stainer <ian.stainer@valtech.co.uk>
2016-10-03 14:49:24 +0100

    VMPRCI-4531 the versionString in the version AT is now Option[String] instead of String so we no longer default it to null

--- acceptance-tests/src/test/scala/gov/uk/dvla/vehicles/dispose/stepdefs/VersionSteps.scala
+++ acceptance-tests/src/test/scala/gov/uk/dvla/vehicles/dispose/stepdefs/VersionSteps.scala
@@ -12,7 +12,7 @@ import gov.uk.dvla.vehicles.dispose.helpers.AcceptanceTestHelper
 import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{WebBrowserDriver, WebDriverFactory}
 
 class VersionSteps(webBrowserDriver: WebBrowserDriver) extends AcceptanceTestHelper {
-  private var versionString: String = null
+  private var versionString: Option[String] = None
 
   implicit val webDriver = webBrowserDriver.asInstanceOf[WebDriver]
 
@@ -23,25 +23,28 @@ class VersionSteps(webBrowserDriver: WebBrowserDriver) extends AcceptanceTestHel
     }).build()
 
     val httpClient = HttpClientBuilder.create.setSSLContext(sslContext).build()
-    val post = new HttpGet(WebDriverFactory.testUrl +  "version")
+    val post = new HttpGet(WebDriverFactory.testUrl + "version")
     val httpResponse = httpClient.execute(post)
-    versionString = fromInputStream(httpResponse.getEntity.getContent).mkString
+    versionString = Option(fromInputStream(httpResponse.getEntity.getContent).mkString)
     httpResponse.close()
   }
 
   @Then("^The user should be able to see version and runtime information for the webapp$")
   def the_user_should_be_able_to_see_version_and_runtime_information_for_the_webapp() = {
-    versionString should include("Name")
-    versionString should include("Version")
-    versionString should include("Build on")
-    versionString should include("Runtime OS")
+    checkData("Name")
+    checkData("Version")
+    checkData("Build on")
+    checkData("Runtime OS")
   }
 
   @Then("^The user should be able to see version and runtime information for the microservices$")
   def the_user_should_be_able_to_see_version_and_runtime_information_for_the_microservices() {
-    versionString should include("email-service")
-    versionString should include("os-address-lookup")
-    versionString should include("vehicle-and-keeper-lookup")
-    versionString should include("vehicles-dispose-fulfil")
+    checkData("email-service")
+    checkData("os-address-lookup")
+    checkData("vehicle-and-keeper-lookup")
+    checkData("vehicles-dispose-fulfil")
   }
+
+  private def checkData(data: String ) =
+    versionString.fold(fail("The version string should be populated")) { vs => vs should include(data) }
 }
